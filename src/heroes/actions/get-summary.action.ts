0f0f55bedf59d9c5dd7ec17dd2147e2fd1d1350d
benjasx <benjasanchez175@gmail.com>
2025-12-19 08:45:19 -0700

    AplicationSuperHeroesv3

--- src/heroes/actions/get-summary.action.ts
+++ src/heroes/actions/get-summary.action.ts
@@ -3,6 +3,5 @@ import type { GetSummary } from "../types/get-summary.response";
 
 export const getSummaryAction = async () => {
   const { data } = await heroApi.get<GetSummary>("/summary");
-
   return data;
 };
--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -48,4 +48,5 @@ export const SearchPage = () => {
     </>
   );
 };
+
 export default SearchPage;
